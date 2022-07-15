6602adfc08b6440167b9df72bbb121ce1b2b3fb4
zidni <zidni.rifan7@gmail.com>
2022-07-15 13:05:29 +0700

    edit ExplorePostList

--- src/components/ExplorePostList.jsx
+++ src/components/ExplorePostList.jsx
@@ -2,9 +2,9 @@ import { ImageList, ImageListItem } from '@mui/material';
 
 export default function ExplorePostList({ posts }) {
   return (
-    <ImageList variant="masonry" cols={3} gap={0} sx={{ marginTop: 0 }}>
+    <ImageList cols={3} rowHeight={140} gap={1} sx={{ marginTop: 0 }}>
       {posts.map((item) => (
-        <ImageListItem sx={{ display: 'block' }} key={item.img}>
+        <ImageListItem key={item.img}>
           <img
             src={`${item.img}?w=248&fit=crop&auto=format`}
             srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}

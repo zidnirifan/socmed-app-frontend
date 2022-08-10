87d67c6973c6853309b8fddc07fe875536d940ad
zidni <zidni.rifan7@gmail.com>
2022-08-10 15:32:12 +0700

    infinite scroll Explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,29 +1,58 @@
+import { Box, CircularProgress, ImageList, ImageListItem } from '@mui/material';
+import React, { useCallback, useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { getExplorePostsMedia } from '../services/api';
 import Navbar from '../components/NavBar';
 import SearchNavbar from '../components/SearchBar';
-import { useCallback, useEffect, useState } from 'react';
-import { getExplorePostsMedia } from '../services/api';
-import ThumbnailList from '../components/ThumbnailList';
+import { useNavigate } from 'react-router-dom';
 
-function Explore() {
-  const [posts, setPosts] = useState([]);
+export default function Explore() {
+  const navigate = useNavigate();
+  const [items, setItems] = useState([]);
 
-  const getHomePostList = useCallback(async () => {
+  const fetchMoreData = async () => {
     const posts = await getExplorePostsMedia();
 
-    setPosts(posts.data.posts);
+    setItems(items.concat(posts.data.posts));
+  };
+
+  const getExplorePosts = useCallback(async () => {
+    const posts = await getExplorePostsMedia();
+
+    setItems(posts.data.posts);
   }, []);
 
   useEffect(() => {
-    getHomePostList();
-  }, [getHomePostList]);
+    getExplorePosts();
+  }, [getExplorePosts]);
 
   return (
     <>
       <Navbar />
       <SearchNavbar />
-      <ThumbnailList posts={posts} />
+      <InfiniteScroll
+        dataLength={items.length}
+        next={fetchMoreData}
+        hasMore={true}
+        loader={
+          <Box sx={{ textAlign: 'center', mb: 10, mt: 3 }}>
+            <CircularProgress size={50} />
+          </Box>
+        }
+      >
+        <ImageList cols={3} rowHeight={140} gap={1} sx={{ marginTop: 0 }}>
+          {items.map((item, i) => (
+            <ImageListItem key={i} onClick={() => navigate(`/post/${item.id}`)}>
+              <img
+                src={item.media}
+                srcSet={item.media}
+                alt={item.id}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </InfiniteScroll>
     </>
   );
 }
-
-export default Explore;

91884a3d95586dae202560daaed66ba3d08e3ac7
zidni <zidni.rifan7@gmail.com>
2022-08-15 17:01:52 +0700

    user not found

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -6,8 +6,10 @@ import {
   ListItemAvatar,
   ListItemButton,
   ListItemText,
+  Typography,
 } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
 export default function UserList({ users, display }) {
   const navigate = useNavigate();
@@ -21,26 +23,33 @@ export default function UserList({ users, display }) {
         display: display,
       }}
     >
-      <List sx={{ padding: 0 }}>
-        {users.map((user) => (
-          <ListItem
-            disablePadding
-            key={user.id}
-            onClick={() => navigate(`/profile/${user.id}`)}
-          >
-            <ListItemButton sx={{ paddingTop: 0.5, paddingBottom: 0.5 }}>
-              <ListItemAvatar>
-                <Avatar aria-label="recipe" src={user.profilePhoto} />
-              </ListItemAvatar>
-              <ListItemText
-                primary={user.username}
-                secondary={user.fullName}
-                sx={{ color: 'black' }}
-              />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+      {users.length === 0 ? (
+        <Box sx={{ textAlign: 'center', marginTop: 1, paddingBottom: 1 }}>
+          <AccountCircleOutlinedIcon sx={{ width: 37, height: 37 }} />
+          <Typography variant="body1">User not found</Typography>
+        </Box>
+      ) : (
+        <List sx={{ padding: 0 }}>
+          {users.map((user) => (
+            <ListItem
+              disablePadding
+              key={user.id}
+              onClick={() => navigate(`/profile/${user.id}`)}
+            >
+              <ListItemButton sx={{ paddingTop: 0.5, paddingBottom: 0.5 }}>
+                <ListItemAvatar>
+                  <Avatar aria-label="recipe" src={user.profilePhoto} />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={user.username}
+                  secondary={user.fullName}
+                  sx={{ color: 'black' }}
+                />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      )}
     </Box>
   );
 }

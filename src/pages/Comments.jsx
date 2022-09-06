c5a0be28db82bdd6f59693d86671f9c25caf1c19
zidni <zidni.rifan7@gmail.com>
2022-09-07 02:44:52 +0700

    skeleton comments

--- src/pages/Comments.jsx
+++ src/pages/Comments.jsx
@@ -5,14 +5,20 @@ import CommentBar from '../components/CommentBar';
 import CommentInput from '../components/CommentInput';
 import CommentList from '../components/CommentList';
 import { useSelector, useDispatch } from 'react-redux';
-import { getComments, commentSelector } from '../redux/features/commentSlice';
+import {
+  getComments,
+  commentSelector,
+  loadingSelector,
+} from '../redux/features/commentSlice';
 import CommentsDisabledIcon from '@mui/icons-material/CommentsDisabled';
+import SkeletonList from '../components/skeleton/SkeletonList';
 
 const Comments = () => {
   const { postId } = useParams();
 
   const dispatch = useDispatch();
   const comments = useSelector(commentSelector.selectAll);
+  const loading = useSelector(loadingSelector);
 
   useEffect(() => {
     dispatch(getComments(postId));
@@ -21,16 +27,20 @@ const Comments = () => {
   return (
     <>
       <CommentBar />
-      {comments.length === 0 ? (
+      {loading ? (
+        <SkeletonList amount={10} />
+      ) : comments.length === 0 ? (
         <Box sx={{ textAlign: 'center', marginTop: 10 }}>
           <CommentsDisabledIcon sx={{ width: 50, height: 50 }} />
           <Typography variant="h5">No Comment</Typography>
         </Box>
       ) : (
-        <CommentList comments={comments} />
+        <>
+          <CommentList comments={comments} />
+          <Box height="50px" />
+        </>
       )}
       <CommentInput />
-      <Box height="50px" />
     </>
   );
 };

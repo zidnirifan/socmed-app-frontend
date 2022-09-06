d3ab7b42ae6c91b4c8506fbd35b98bb1865b3b5e
zidni <zidni.rifan7@gmail.com>
2022-09-07 01:53:45 +0700

    skeleton profile

--- src/pages/OwnProfile.jsx
+++ src/pages/OwnProfile.jsx
@@ -17,15 +17,20 @@ import { getProfile as getProfileApi } from '../services/api';
 import { useNavigate } from 'react-router-dom';
 import OwnProfileBar from '../components/ownProfileBar';
 import CameraAltOutlinedIcon from '@mui/icons-material/CameraAltOutlined';
+import { getLocalUser } from '../services/token';
+import SkeletonProfile from '../components/SkeletonProfile';
 
 function OwnProfile() {
   const navigate = useNavigate();
+  const { id } = getLocalUser();
 
+  const [loading, setLoading] = useState(true);
   const [user, setUser] = useState({});
 
   const getProfile = useCallback(async () => {
     const userData = await getProfileApi('');
 
+    setLoading(false);
     setUser(userData.data.userProfile);
   }, []);
 
@@ -37,95 +42,123 @@ function OwnProfile() {
     <>
       <Navbar />
       <OwnProfileBar username={user.username} />
-      <Paper
-        elevation={0}
-        sx={{
-          p: 2,
-          margin: 'auto',
-          maxWidth: 500,
-          flexGrow: 1,
-          paddingBottom: 1,
-        }}
-      >
-        <Grid container spacing={2}>
-          <Grid item xs={4}>
-            <Avatar
-              sx={{ width: 90, height: 90, margin: 'auto' }}
-              src={user.profilePhoto}
-            />
-          </Grid>
-          <Grid item xs={8} container>
-            <Grid item xs={4} sx={{ paddingTop: 3.5 }}>
-              <Typography
-                variant="subtitle1"
-                textAlign="center"
-                sx={{ fontWeight: 500, lineHeight: 1 }}
-              >
-                {user.postsCount}
-              </Typography>
-              <Typography component="p" variant="caption" textAlign="center">
-                Posts
-              </Typography>
-            </Grid>
-            <Grid item xs={4} sx={{ paddingTop: 3.5 }}>
-              <Typography
-                variant="subtitle1"
-                textAlign="center"
-                sx={{ fontWeight: 500, lineHeight: 1 }}
-              >
-                {user.followersCount}
-              </Typography>
-              <Typography component="p" variant="caption" textAlign="center">
-                Followers
-              </Typography>
-            </Grid>
-            <Grid item xs={4} sx={{ paddingTop: 3.5 }}>
-              <Typography
-                variant="subtitle1"
-                textAlign="center"
-                sx={{ fontWeight: 500, lineHeight: 1 }}
-              >
-                {user.followingCount}
-              </Typography>
-              <Typography component="p" variant="caption" textAlign="center">
-                Following
-              </Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Paper>
-      <Container>
-        <Typography variant="subtitle2">{user.fullName}</Typography>
-        <Typography variant="subtitle2" color={grey[500]}>
-          {user.bio}
-        </Typography>
-        <Box sx={{ marginTop: 2, padding: '0 15%' }}>
-          <Button
-            variant="outlined"
-            size="small"
-            sx={{ width: '100%' }}
-            onClick={() => navigate('/edit-profile')}
-          >
-            Edit Profile
-          </Button>
-        </Box>
-      </Container>
-      <Tabs
-        value={0}
-        variant="fullWidth"
-        aria-label="icon position tabs example"
-      >
-        <Tab icon={<GridIcon />} />
-      </Tabs>
-      {user.posts?.length === 0 ? (
-        <Box sx={{ textAlign: 'center', marginTop: 5 }}>
-          <CameraAltOutlinedIcon sx={{ width: 70, height: 70 }} />
-          <Typography variant="h5" sx={{ fontWeight: 500 }}>
-            No posts yet
-          </Typography>
-        </Box>
+      {loading ? (
+        <SkeletonProfile />
       ) : (
-        <ThumbnailList posts={user.posts || []} />
+        <>
+          <Paper
+            elevation={0}
+            sx={{
+              p: 2,
+              margin: 'auto',
+              maxWidth: 500,
+              flexGrow: 1,
+              paddingBottom: 1,
+            }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={4}>
+                <Avatar
+                  sx={{ width: 90, height: 90, margin: 'auto' }}
+                  src={user.profilePhoto}
+                />
+              </Grid>
+              <Grid item xs={8} container>
+                <Grid item xs={4} sx={{ paddingTop: 3.5 }}>
+                  <Typography
+                    variant="subtitle1"
+                    textAlign="center"
+                    sx={{ fontWeight: 500, lineHeight: 1 }}
+                  >
+                    {user.postsCount}
+                  </Typography>
+                  <Typography
+                    component="p"
+                    variant="caption"
+                    textAlign="center"
+                  >
+                    Posts
+                  </Typography>
+                </Grid>
+                <Grid
+                  item
+                  xs={4}
+                  sx={{ paddingTop: 3.5 }}
+                  onClick={() => navigate(`/${id}/followers`)}
+                >
+                  <Typography
+                    variant="subtitle1"
+                    textAlign="center"
+                    sx={{ fontWeight: 500, lineHeight: 1 }}
+                  >
+                    {user.followersCount}
+                  </Typography>
+                  <Typography
+                    component="p"
+                    variant="caption"
+                    textAlign="center"
+                  >
+                    Followers
+                  </Typography>
+                </Grid>
+                <Grid
+                  item
+                  xs={4}
+                  sx={{ paddingTop: 3.5 }}
+                  onClick={() => navigate(`/${id}/following`)}
+                >
+                  <Typography
+                    variant="subtitle1"
+                    textAlign="center"
+                    sx={{ fontWeight: 500, lineHeight: 1 }}
+                  >
+                    {user.followingCount}
+                  </Typography>
+                  <Typography
+                    component="p"
+                    variant="caption"
+                    textAlign="center"
+                  >
+                    Following
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Paper>
+          <Container>
+            <Typography variant="subtitle2">{user.fullName}</Typography>
+            <Typography variant="subtitle2" color={grey[500]}>
+              {user.bio}
+            </Typography>
+            <Box sx={{ marginTop: 2, padding: '0 15%' }}>
+              <Button
+                variant="outlined"
+                size="small"
+                sx={{ width: '100%' }}
+                onClick={() => navigate('/edit-profile')}
+              >
+                Edit Profile
+              </Button>
+            </Box>
+          </Container>
+          <Tabs
+            value={0}
+            variant="fullWidth"
+            aria-label="icon position tabs example"
+          >
+            <Tab icon={<GridIcon />} />
+          </Tabs>
+          {user.posts?.length === 0 ? (
+            <Box sx={{ textAlign: 'center', marginTop: 5 }}>
+              <CameraAltOutlinedIcon sx={{ width: 70, height: 70 }} />
+              <Typography variant="h5" sx={{ fontWeight: 500 }}>
+                No posts yet
+              </Typography>
+            </Box>
+          ) : (
+            <ThumbnailList posts={user.posts || []} />
+          )}
+        </>
       )}
     </>
   );
